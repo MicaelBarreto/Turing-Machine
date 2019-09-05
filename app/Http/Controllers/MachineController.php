528c9999ebd064a56c81ac56e99e49b6238334bd
Micaelbarreto <micaelplayer98@gmail.com>
2019-09-05 12:46:51 -0300

    Machine controller

--- app/Http/Controllers/MachineController.php
+++ app/Http/Controllers/MachineController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\TuringMachine;
+use App\Services\RunMachine;
+
+use validator;
+use DB;
 
 class MachineController extends Controller
 {
@@ -13,15 +18,46 @@ class MachineController extends Controller
     }
 
     public function store(Request $request){
-        // $validatedData = $request->validate([
-        //     'name' => 'required',
-        //     'description' => 'required',
-        // ]);
+        $v = $request->validate([
+            'q' => 'String|required',
+            'sigma' => 'String|required',
+            'gamma' => 'String|required',
+            'delta' => 'String|required',
+            'q0' => 'String|required',
+            'vazio' => 'String|required',
+            'f' => 'String|required',
+            'word' => 'String|required',
+        ]);
 
-        // $project = Project::create([
-        //     'name' => $validatedData['name'],
-        //     'description' => $validatedData['description'],
-        // ]);
+        if($v->fails()){
+            return response()->json($e);
+        }
+
+        DB::beginTransaction();
+        try{
+            $machine = new TuringMachine;
+            $machine->q = $request->q;
+            $machine->sigma = $request->sigma;
+            $machine->gamma = $request->gamma;
+            $machine->delta = $request->delta;
+            $machine->q0 = $request->q0;
+            $machine->vazio = $request->vazio;
+            $machine->f = $request->f;
+            $machine->word = $request->word;
+
+            $res = RunMachine::run($machine);
+
+            $machine->res = $res;
+
+            $machine->save();
+            DB::commit();
+            
+            return reponse()->json($machine->res);
+        }catch(\PDOException $e){
+            DB::rollBack();
+
+            return reponse()->json($e);
+        }
 
         return response()->json('Project created!');
     }
---
+++ app/Services/RunMachine.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Services;
+
+
+class RunMachine{
+    public static function run($machine){
+        
+        
+        return true;
+    }
+
+}
